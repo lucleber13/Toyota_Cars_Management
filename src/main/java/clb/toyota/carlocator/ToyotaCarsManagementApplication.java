1d9c9ae1af6e98d893b0849c2e5801f8a8d580e8
lucleber13 <lucleber13@hotmail.com>
2022-04-28 01:14:28 +0100

    first commit

---
+++ src/main/java/clb/toyota/carlocator/ToyotaCarsManagementApplication.java
@@ -0,0 +1,13 @@
+package clb.toyota.carlocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyotaCarsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyotaCarsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clb/toyota/carlocator/ToyotaCarsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package clb.toyota.carlocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyotaCarsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
